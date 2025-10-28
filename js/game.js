da29162d70c08c0b4c933bfa7b17e54586a7a0cd
yKiwiizin <otaviooo0805@gmail.com>
2025-10-27 23:41:22 -0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,103 +1,94 @@
-const grid = document.querySelector('.grid');
-const spanPlayer = document.querySelector('.player');
-const timer = document.querySelector('.timer');
+const grid = document.querySelector(".grid");
+const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
 
 const characters = [
-  'beth',
-  'jerry',
-  'jessica',
-  'morty',
-  'pessoa-passaro',
-  'pickle-rick',
-  'rick',
-  'summer',
-  'meeseeks',
-  'scroopy',
+  "beth",
+  "jerry",
+  "jessica",
+  "morty",
+  "pessoa-passaro",
+  "pickle-rick",
+  "rick",
+  "summer",
+  "meeseeks",
+  "scroopy",
 ];
 
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
   element.className = className;
   return element;
-}
+};
 
-let firstCard = '';
-let secondCard = '';
+let firstCard = "";
+let secondCard = "";
 
 const checkEndGame = () => {
-  const disabledCards = document.querySelectorAll('.disabled-card');
+  const disabledCards = document.querySelectorAll(".disabled-card");
 
   if (disabledCards.length === 20) {
     clearInterval(this.loop);
-    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`);
+    alert(
+      `Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`
+    );
   }
-}
+};
 
 const checkCards = () => {
-  const firstCharacter = firstCard.getAttribute('data-character');
-  const secondCharacter = secondCard.getAttribute('data-character');
+  const firstCharacter = firstCard.getAttribute("data-character");
+  const secondCharacter = secondCard.getAttribute("data-character");
 
   if (firstCharacter === secondCharacter) {
+    firstCard.firstChild.classList.add("disabled-card");
+    secondCard.firstChild.classList.add("disabled-card");
 
-    firstCard.firstChild.classList.add('disabled-card');
-    secondCard.firstChild.classList.add('disabled-card');
-
-    firstCard = '';
-    secondCard = '';
+    firstCard = "";
+    secondCard = "";
 
     checkEndGame();
-
   } else {
     setTimeout(() => {
+      firstCard.classList.remove("reveal-card");
+      secondCard.classList.remove("reveal-card");
 
-      firstCard.classList.remove('reveal-card');
-      secondCard.classList.remove('reveal-card');
-
-      firstCard = '';
-      secondCard = '';
-
+      firstCard = "";
+      secondCard = "";
     }, 500);
   }
-
-}
+};
 
 const revealCard = ({ target }) => {
-
-  if (target.parentNode.className.includes('reveal-card')) {
+  if (target.parentNode.className.includes("reveal-card")) {
     return;
   }
 
-  if (firstCard === '') {
-
-    target.parentNode.classList.add('reveal-card');
+  if (firstCard === "") {
+    target.parentNode.classList.add("reveal-card");
     firstCard = target.parentNode;
-
-  } else if (secondCard === '') {
-
-    target.parentNode.classList.add('reveal-card');
+  } else if (secondCard === "") {
+    target.parentNode.classList.add("reveal-card");
     secondCard = target.parentNode;
 
     checkCards();
-
   }
-}
+};
 
 const createCard = (character) => {
-
-  const card = createElement('div', 'card');
-  const front = createElement('div', 'face front');
-  const back = createElement('div', 'face back');
+  const card = createElement("div", "card");
+  const front = createElement("div", "face front");
+  const back = createElement("div", "face back");
 
   front.style.backgroundImage = `url('../images/${character}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
 
-  card.addEventListener('click', revealCard);
-  card.setAttribute('data-character', character)
+  card.addEventListener("click", revealCard);
+  card.setAttribute("data-character", character);
 
   return card;
-}
+};
 
 const loadGame = () => {
   const duplicateCharacters = [...characters, ...characters];
@@ -108,19 +99,17 @@ const loadGame = () => {
     const card = createCard(character);
     grid.appendChild(card);
   });
-}
+};
 
 const startTimer = () => {
-
   this.loop = setInterval(() => {
     const currentTime = +timer.innerHTML;
     timer.innerHTML = currentTime + 1;
   }, 1000);
-
-}
+};
 
 window.onload = () => {
-  spanPlayer.innerHTML = localStorage.getItem('player');
+  spanPlayer.innerHTML = localStorage.getItem("player");
   startTimer();
   loadGame();
-}
+};
